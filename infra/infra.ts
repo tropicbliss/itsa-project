1904ce1ee301f2a7b34db694393481696b40fb91
tropicbliss <tropicbliss@protonmail.com>
2024-09-08 15:49:13 +0800

    Add frontend

--- infra/infra.ts
+++ infra/infra.ts
@@ -52,3 +52,18 @@ api.route("GET /", {
     }
   }
 });
+
+export const frontend = new sst.aws.StaticSite("Frontend", {
+  path: "packages/frontend",
+  build: {
+    output: "dist",
+    command: "npm run build",
+  },
+  environment: {
+    VITE_REGION: region,
+    VITE_API_URL: api.url,
+    VITE_USER_POOL_ID: userPool.id,
+    VITE_IDENTITY_POOL_ID: identityPool.id,
+    VITE_USER_POOL_CLIENT_ID: userPoolClient.id,
+  },
+});
