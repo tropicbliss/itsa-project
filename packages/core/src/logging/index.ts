6426afd3766a782b1b4fb09fc330d93d83f53d0a
tropicbliss <tropicbliss@protonmail.com>
2024-10-13 12:57:29 +0800

    update iam actions for cloudwatch

--- packages/core/src/logging/index.ts
+++ packages/core/src/logging/index.ts
@@ -12,21 +12,21 @@ const client = new CloudWatchLogsClient({ region: Resource.Region.name });
 
 export namespace Log {
   export async function createClient(data: Common) {
-    await logRaw({
+    await logLambdaRaw({
       ...data,
       crud: "create",
     });
   }
 
   export async function readClient(data: Common) {
-    await logRaw({
+    await logLambdaRaw({
       ...data,
       crud: "read",
     });
   }
 
   export async function deleteClient(data: Common) {
-    await logRaw({
+    await logLambdaRaw({
       ...data,
       crud: "delete",
     });
@@ -35,7 +35,7 @@ export namespace Log {
   export async function updateClient(
     data: Common & { attributes: Record<string, AttributeValue> }
   ) {
-    await logRaw({
+    await logLambdaRaw({
       crud: "update",
       ...data,
     });
@@ -48,112 +48,89 @@ export namespace Log {
     },
     timestamp: string
   ) {
-    const logGroup = Resource.CommunicationLogGroup.name;
-    await client.send(
-      new CreateLogStreamCommand({
-        logGroupName: logGroup,
-        logStreamName: LOG_STREAM_NAME,
-      })
+    await logRaw(
+      Resource.CommunicationLogGroup.name,
+      { type: "email", ...data },
+      new Date(timestamp)
     );
-    const { logStreams } = await client.send(
-      new DescribeLogStreamsCommand({
-        logGroupName: logGroup,
-        logStreamNamePrefix: LOG_STREAM_NAME,
-      })
-    );
-    const sequenceToken =
-      logStreams && logStreams.length > 0
-        ? logStreams[0].uploadSequenceToken
-        : undefined;
-    await client.send(
-      new PutLogEventsCommand({
-        logGroupName: logGroup,
-        logStreamName: LOG_STREAM_NAME,
-        logEvents: [
-          {
-            message: JSON.stringify({
-              type: "email",
-              ...data,
-            }),
-            timestamp: new Date(timestamp).getTime(),
-          },
-        ],
-        sequenceToken,
-      })
-    );
+  }
+
+  type Common = {
+    agentId: string;
+    clientId: string;
+  };
+
+  type Create = "create";
+  type Read = "read";
+  type Delete = "delete";
+  type Update = "update";
+
+  type CRD = {
+    crud: Create | Read | Delete;
+  } & Common;
+
+  type U = {
+    crud: Update;
+    attributes: Record<string, AttributeValue>;
+  } & Common;
+
+  export type AttributeValue = {
+    beforeValue: unknown;
+    afterValue: unknown;
+  };
+
+  type Data = CRD | U;
+
+  type Output = {
+    crud: Create | Read | Update | Delete;
+    attributeName: string;
+    beforeValue?: string;
+    afterValue?: string;
+  } & Common;
+
+  async function logLambdaRaw(data: Data) {
+    const logGroup = Resource.LambdaLogGroup.name;
+    let output: Output;
+    switch (data.crud) {
+      case "update":
+        output = {
+          crud: data.crud,
+          agentId: data.agentId,
+          attributeName: Object.keys(data.attributes).join("|"),
+          clientId: data.clientId,
+          afterValue: Object.values(data.attributes)
+            .map((val) => JSON.stringify(val.afterValue))
+            .join("|"),
+          beforeValue: Object.values(data.attributes)
+            .map((val) => JSON.stringify(val.beforeValue))
+            .join("|"),
+        };
+        break;
+      case "read":
+      case "create":
+      case "delete":
+        output = {
+          crud: data.crud,
+          agentId: data.agentId,
+          attributeName: data.clientId,
+          clientId: data.clientId,
+        };
+    }
+    await logRaw(Resource.LambdaLogGroup.name, output);
   }
 }
 
-type Common = {
-  agentId: string;
-  clientId: string;
-};
-
-type Create = "create";
-type Read = "read";
-type Delete = "delete";
-type Update = "update";
-
-type CRD = {
-  crud: Create | Read | Delete;
-} & Common;
-
-type U = {
-  crud: Update;
-  attributes: Record<string, AttributeValue>;
-} & Common;
-
-export type AttributeValue = {
-  beforeValue: unknown;
-  afterValue: unknown;
-};
-
-type Data = CRD | U;
-
-type Output = {
-  crud: Create | Read | Update | Delete;
-  attributeName: string;
-  beforeValue?: string;
-  afterValue?: string;
-} & Common;
-
-async function logRaw(data: Data) {
-  const logGroup = Resource.LambdaLogGroup.name;
-  let output: Output;
-  switch (data.crud) {
-    case "update":
-      output = {
-        crud: data.crud,
-        agentId: data.agentId,
-        attributeName: Object.keys(data.attributes).join("|"),
-        clientId: data.clientId,
-        afterValue: Object.values(data.attributes)
-          .map((val) => JSON.stringify(val.afterValue))
-          .join("|"),
-        beforeValue: Object.values(data.attributes)
-          .map((val) => JSON.stringify(val.beforeValue))
-          .join("|"),
-      };
-      break;
-    case "read":
-    case "create":
-    case "delete":
-      output = {
-        crud: data.crud,
-        agentId: data.agentId,
-        attributeName: data.clientId,
-        clientId: data.clientId,
-      };
-  }
+async function logRaw(groupName: string, data: object, timestamp?: Date) {
+  const LOG_STREAM_NAME = "main";
   await client.send(
     new CreateLogStreamCommand({
-      logGroupName: logGroup,
+      logGroupName: groupName,
       logStreamName: LOG_STREAM_NAME,
     })
   );
   const { logStreams } = await client.send(
     new DescribeLogStreamsCommand({
-      logGroupName: logGroup,
+      logGroupName: groupName,
       logStreamNamePrefix: LOG_STREAM_NAME,
     })
   );
@@ -163,12 +140,12 @@ async function logRaw(data: Data) {
       : undefined;
   await client.send(
     new PutLogEventsCommand({
-      logGroupName: logGroup,
+      logGroupName: groupName,
       logStreamName: LOG_STREAM_NAME,
       logEvents: [
         {
-          message: JSON.stringify(output),
-          timestamp: new Date().getTime(),
+          message: JSON.stringify(data),
+          timestamp: (timestamp ?? new Date()).getTime(),
         },
       ],
       sequenceToken,
