a6354b9bac6baf217138037cf7d972a99d4cf2a9
tropicbliss <tropicbliss@protonmail.com>
2024-10-15 01:02:40 +0800

    Remove email topic

--- nightmaregen/index.py
+++ nightmaregen/index.py
@@ -4,14 +4,29 @@ import json
 import uuid
 import random
 from datetime import datetime
+from collections import deque
 
 url = "https://dglm65hpgq6w7kpxaxxugbypqy0gjdni.lambda-url.ap-southeast-1.on.aws/"
 
+pending = deque(maxlen=27)
+
 while True:
-    with urllib.request.urlopen(url) as response:
-        data = response.read().decode("utf-8")
-        account_id = json.loads(data)
+    time.sleep(27)
+    account_id = None
+    if random.choice([True, False]):
+        with urllib.request.urlopen(url) as response:
+            data = response.read().decode("utf-8")
+            account_id = json.loads(data)
+    else:
+        try:
+            account_id = pending.pop()
+        except:
+            pass
     if account_id != None:
+        random_status = random.choice(["completed", "pending", "failed"])
+        if random_status == "pending":
+            pending.append(account_id)
+            continue
         generated_uuid = str(uuid.uuid4())
         output = {
             "id": generated_uuid,
@@ -19,8 +34,7 @@ while True:
             "type": random.choice(["D", "W"]),
             "amount": round(random.uniform(0, 100000), 2),
             "date": datetime.now().strftime("%Y-%m-%d"),
-            "status": "completed"
+            "status": random_status
         }
         with open(f"processing/{generated_uuid}.txt", 'w') as file:
             json.dump(output, file)
-    time.sleep(27)
